da956beed3de90420d6f5c221d920187c939b597
Murray Campbell <MurrayIRC@gmail.com>
2021-05-06 09:01:59 -0700

    moved catlike coding tutorials into a separate project
    no sense in putting them in here. if i need something from them i'll just copy it over

--- Assets/CatlikeCodingTutorials/ObjectManagement/Scripts/RotatingObject.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class RotatingObject : PersistentObject {
-    [SerializeField] private Vector3 angularVelocity;
-
-    private void FixedUpdate() {
-        transform.Rotate(angularVelocity * Time.deltaTime);
-    }
-}
